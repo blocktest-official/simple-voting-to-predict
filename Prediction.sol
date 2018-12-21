792ad0b36effb76999728428b7fc635603613e78
blocktest-official <43646822+blocktest-official@users.noreply.github.com>
2018-12-21 17:05:08 -0500

    Updated Prediction.sol -> using keccak256

--- Prediction.sol
+++ Prediction.sol
@@ -20,11 +20,12 @@ contract Prediction {
         // making sure only owner can post the predictions
         require (tx.origin == owner, "Only owner can post predictions!");
         
-        // making sure this is user's first prediction (unique)
-        if (bytes(predictionMap[StringAsKey.convert(blockTESTUserId)]).length == 0)
+        // making sure this is user's first prediction (unique)    bytes4(keccak256("fun(uint256)")), a
+        // if (bytes(predictionMap[StringAsKey.convert(blockTESTUserId)]).length == 0)
+        if (bytes(predictionMap[(keccak256(blockTESTUserId))]).length == 0)
             uniquePredictions += 1;
         predictions += 1;
-        predictionMap[StringAsKey.convert(blockTESTUserId)] = val;
+        predictionMap[keccak256(blockTESTUserId)] = val;
         
         // emitting event
         emit PredictionEvent(blockTESTUserId, val, predictions, uniquePredictions);
@@ -32,18 +33,6 @@ contract Prediction {
     
     // access this function to get your prediction
     function getPrediction(string memory blockTESTUserId) public view returns (string memory) {
-        return predictionMap[StringAsKey.convert(blockTESTUserId)];
-    }
-}
-
-library StringAsKey {
-    function convert(string memory key) public pure returns (bytes32 ret) {
-        if (bytes(key).length > 32) {
-          revert("Key length > 32!");
-        }
-    
-        assembly {
-          ret := mload(add(key, 32))
-        }
+        return predictionMap[keccak256(blockTESTUserId)];
     }
 }
